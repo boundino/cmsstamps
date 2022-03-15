711378f3c941cbc4c89db55fcb2f18f938bd6da5
boundino <boundino@gmail.com>
2022-03-15 17:52:26 -0400

    first preliminary version

--- svg.js
+++ svg.js
@@ -1,14 +1,17 @@
 var lcanvas, svg;
-function w() { return lcanvas*0.8; }
+function w() { return lcanvas*0.95; }
 function h() { return w()/4; }
 function theta() { return 15; }
 function stroke_width() { return 6; }
-function color() { return "rgb(0,0,0)"; }
-
-function setAttributes(el, attrs) {
-    for(var key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
+function color() { return document.getElementById('colorpicker').value; }
+function reset() {
+    document.getElementById('inputt').value = 'CMS';
+    document.getElementById('inputb').value = 'QM22';
+    document.getElementById('inputm').value = 'NEW RESULT';
+    document.getElementById('slidert').value = '150';
+    document.getElementById('sliderb').value = '160';
+    document.getElementById('sliderm').value = '40';
+    document.getElementById('colorpicker').value = '#A51D1D';
 }
 
 function draw() {
@@ -17,6 +20,7 @@ function draw() {
 
     drawrect();
     drawarcgroup(w()/2*0.9);
+    setattr();
 }
 
 function drawrect() {
@@ -27,7 +31,8 @@ function drawrect() {
     rect.setAttribute("ry", "10");
     rect.setAttribute("width", w());
     rect.setAttribute("height", h());
-    rect.setAttribute("style", "fill:transparent; stroke-width:"+stroke_width()+"; stroke:"+color()+";");
+    rect.setAttribute("style", "fill:transparent; stroke-width:"+stroke_width()+";");
+    rect.setAttribute("class", "graph");
     rect.setAttribute("transform", "rotate(-"+theta()+" "+lcanvas*0.5+","+lcanvas*0.5+")");
     svg.appendChild(rect);
 }
@@ -45,13 +50,15 @@ function drawarc(R, id) {
 
             let arc = document.createElementNS("http://www.w3.org/2000/svg", "path");
             arc.setAttribute("d", "M "+x1+","+y1+" A "+R+","+R+" 0 0,1 "+x2+","+y2);
-            arc.setAttribute("style", "fill:transparent; stroke-width:"+stroke_width()+"; stroke:"+color()+";");
+            arc.setAttribute("style", "fill:transparent; stroke-width:"+stroke_width()+";");
+            arc.setAttribute("class", "graph");
             arc.setAttribute("id", id+"t");
             svg.appendChild(arc);
 
             let arc2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
             arc2.setAttribute("d", "M "+(lcanvas-x2)+","+(lcanvas-y2)+" A "+R+","+R+" 0 0,0 "+(lcanvas-x1)+","+(lcanvas-y1));
-            arc2.setAttribute("style", "fill:transparent; stroke-width:"+stroke_width()+"; stroke:"+color()+";");
+            arc2.setAttribute("style", "fill:transparent; stroke-width:"+stroke_width()+";");
+            arc2.setAttribute("class", "graph");
             arc2.setAttribute("id", id+"b");
             svg.appendChild(arc2);
         }
@@ -59,7 +66,35 @@ function drawarc(R, id) {
 
 function drawarcgroup(R) {
     drawarc(R, "");
-    drawarc(R-stroke_width()*0.95, "");
+    drawarc(R-stroke_width()*0.9, "");
     drawarc(R-stroke_width()*3, "inner");
     drawarc(h()/2*1.5, "");
 }
+
+function setattr() {
+    var tsize = w()*0.14;
+    let textt = document.getElementById('textt');
+    textt.innerHTML = document.getElementById('inputt').value;
+    textt.style.fontSize = tsize+"px";
+    textt.setAttribute("textLength", document.getElementById('slidert').value);
+    let textb = document.getElementById('textb');
+    textb.innerHTML = document.getElementById('inputb').value;
+    textb.style.fontSize = tsize+"px";
+    textb.setAttribute("textLength", document.getElementById('sliderb').value);
+    let textm = document.getElementById('textm');
+    textm.innerHTML = document.getElementById('inputm').value;
+    textm.setAttribute("x", lcanvas*0.5);
+    textm.setAttribute("y", lcanvas*0.5);
+    textm.setAttribute("text-anchor", "middle");
+    textm.setAttribute("alignment-baseline", "middle");
+    textm.setAttribute("transform", "rotate(-"+theta()+" "+lcanvas*0.5+","+lcanvas*0.5+")");
+    textm.style.fontSize = (tsize/40*document.getElementById('sliderm').value) + "px";
+
+    let graphs = document.getElementsByClassName('graph');
+    for(var i=0; i<graphs.length; i++)
+        graphs[i].style.stroke = color();
+    let texts = document.getElementsByTagName('text');
+    for(var i=0; i<texts.length; i++)
+        texts[i].style.fill = color();
+}
+
